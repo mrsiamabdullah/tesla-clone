05946af6619331e9c4e235385fc43490eaa203fc
mrsiamabdullah <mrsiamabdullah@gmail.com>
2024-04-26 00:24:48 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// OPEN SIDE MENU
+document.getElementById("menu").addEventListener("click", () => {
+  document.getElementById("sideNav").classList.remove("right-[-50%]");
+  document.getElementById("sideNav").classList.add("right-0");
+  document.getElementById("mainDiv").classList.add("blur-sm");
+});
+
+// CLOSE SIDE MENU
+document.getElementById("closeIcon").addEventListener("click", () => {
+  document.getElementById("sideNav").classList.add("right-[-50%]");
+  document.getElementById("sideNav").classList.remove("right-0");
+  document.getElementById("mainDiv").classList.remove("blur-sm");
+});
+
+// SCROLL SECTIONS ACTIVE LINK
+let sections = document.querySelectorAll("section[id]");
+
+let scrollActive = () => {
+  let scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    let sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav-menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
